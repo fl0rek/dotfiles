9063bb10bff4a84df583047a3c5a79034dde7cec
Mikołaj Florkiewicz <florek@florkiewicz.me>
2016-09-23 05:08:59 +0200

    next iteration of install scripts

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
+HOSTNAME=`hostname`
 
 PWD=`pwd`
 STOW=`which stow`
 
-DIRECTORIES=".config .ssh"
-PACKAGES="git i3 monoid-font mutt powerline shell ssh tmux vim zsh"
-
-if [[ ! -f $STOW ]] ; then
-	pushd stow-1.3.2
-	./configure
-	STOW="perl $PWD/stow"
-	popd
-if
+PACKAGES=`cat config`
 
 for p in $PACKAGES ; do
 	$STOW $p
