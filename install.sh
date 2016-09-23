0ab949dbf035c48223fe02919abc04542baad8eb
Mikołaj Florkiewicz <florek@florkiewicz.me>
2016-09-23 05:32:42 +0200

    gitignore for dynamically generated files, new install script

--- install.sh
+++ install.sh
@@ -2,11 +2,27 @@
 
 HOSTNAME=`hostname`
 
-PWD=`pwd`
 STOW=`which stow`
+if [[ ! -f $STOW ]] ; then
+	echo No stow found 1>&2
+	exit
+fi
+M4=`which m4`
+if [[ ! -f $M4 ]] ; then
+	echo No m4 found 1>&2
+	exit
+fi
 
-PACKAGES=`cat config`
+M4_FILES=`find -iname \*m4`
 
+for INPUT in $M4_FILES ; do
+	OUTPUT=${INPUT%.m4}
+	echo $INPUT '=>' $OUTPUT
+	$M4 -P -D HOSTNAME=$HOSTNAME $INPUT > $OUTPUT
+done
+exit
+
+PACKAGES=`cat configs`
 for p in $PACKAGES ; do
 	$STOW $p
 done
